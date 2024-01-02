9c9630c374b913658e6bce1ca3567bc63bf6b836
kavinskarasu <kavinskarasu@gmail.com>
2024-01-02 13:53:13 +0530

    added .env

--- config/config.js
+++ config/config.js
@@ -1,5 +1,7 @@
-module.exports.mongourl =
-  "mongodb+srv://KAVIARASU:KAVIARASU@cluster0.rnxze.mongodb.net/blog?retryWrites=true&w=majority";
+const dotenv = require("dotenv");
+dotenv.config();
+
+module.exports.mongourl = process.env.DB;
 module.exports.PORT = process.env.PORT || 5000;
 module.exports.globalVariables = (req, res, next) => {
   res.locals.success_message = req.flash("success-message");
